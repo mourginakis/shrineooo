db651eebdf0024c3104593f4ee77f2ece6bd6064
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2025-09-21 07:07:24 -0700

    create secrets_.py

---
+++ secrets_.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+##############################################
+## Secrets
+
+# Postgres DB (Digital Ocean)
+POSTGRES_USERNAME      = os.environ.get('POSTGRES_USERNAME',   '')
+POSTGRES_PASSWORD      = os.environ.get('POSTGRES_PASSWORD',   '')
+POSTGRES_HOST          = os.environ.get('POSTGRES_HOST',       '')
+POSTGRES_PORT          = os.environ.get('POSTGRES_PORT',       '')
+POSTGRES_DATABASE      = os.environ.get('POSTGRES_DATABASE',   '')
+POSTGRES_CREDENTIALS = {
+    "user": POSTGRES_USERNAME,
+    "password": POSTGRES_PASSWORD,
+    "host": POSTGRES_HOST,
+    "port": POSTGRES_PORT,
+    "dbname": POSTGRES_DATABASE,
+}
