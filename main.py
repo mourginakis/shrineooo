3b236c27501570566d0f01922ca36280b1fcd1d9
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2025-09-21 07:33:21 -0700

    create sql demos with psycopg2 and sqlalchemy

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python3.12
 
 #%%
+from pprint import pprint
+from secrets_ import POSTGRES_CREDENTIALS, POSTGRES_URL
 
-print("I don't do much, yet...")
+
+# %%
+# psycopg2 example
+# cur.fetchall() - gets data from SELECT queries
+# conn.commit() - saves data from INSERT/UPDATE/DELETE/CREATE
+import psycopg2
+conn = psycopg2.connect(**POSTGRES_CREDENTIALS)
+cur = conn.cursor()
+cur.execute("SELECT * FROM xusers")
+records = cur.fetchall()
+cur.close()
+conn.close()
+pprint(records)
+
+
+
+#%%
+# sqlalchemy example (without pandas)
+from sqlalchemy import create_engine, text
+import pandas as pd
+
+# without pandas
+engine = create_engine(POSTGRES_URL)
+connection = engine.connect()
+result = connection.execute(text("SELECT * FROM xusers"))
+records = result.fetchall()
+connection.close()
+pprint(records)
+
+#%%
+# sqlalchemy example (with pandas)
+import pandas as pd
+connection = engine.connect()
+df = pd.read_sql_query(text("SELECT * FROM xusers"), connection)
+pprint(df)
+connection.close()
 
 
 # %%
