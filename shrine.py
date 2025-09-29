953c58f9883bc6a1abbc61c3f9bc10a795dc7c17
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2025-09-29 05:11:29 -0700

    create hydrate_webprint_distill1s

--- shrine.py
+++ shrine.py
@@ -4,84 +4,12 @@
 from pprint import pprint
 from sqlalchemy import create_engine, text
 
-from src.api_chatbot import gpt5_web, gpt5_web_flex_mini
+from src.api_chatbot import gpt5_web, gpt5_web_flex_mini, gpt5
 from src.secrets_ import POSTGRES_CREDENTIALS, POSTGRES_URL
 engine = create_engine(POSTGRES_URL)
 
 # sqlalchemy rawsql is preferred over psycopg2 because the API
 # is more readable and it auto manages connection pools.
-
-#%% ========================================
-
-
-
-def seed_notes():
-    # 1. get 2 coins from the cmcmaster table where status is active and cmcnotes.note is NULL, ordered ascending by rank
-
-    sql = \
-"""
-SELECT m.id, m.name, m.urls
-FROM cmcmaster AS m
-JOIN cmcnotes  AS n ON n.id = m.id
-WHERE m.status = 'active'
-  AND n.note IS NULL
-ORDER BY m.rank ASC NULLS LAST
-LIMIT 2;
-"""
-    conn = engine.connect()
-    rows = conn.execute(text(sql))
-    rows = rows.mappings().fetchall()
-    conn.close()
-    print(f"got: {str([row['name'] for row in rows])}")
-    # print(f"Found {len(rows)} ids")
-    # pprint(rows)
-    # return rows
-
-    for row in rows:
-        prompt = f"""
-        Please write a research report on the following coin:
-        {row['name']}
-
-        I have aggregated a number of links that you can use as reference, if you can't
-        find information on your own. Some of these links are for things like telegram and twiter and stuff,
-        and might entirely be worth ignoring. But the websites and technical stuff might be worth reading.
-
-        The goal here is to create a "fingerprint" for this token, to explore in-depth what the crypto 
-        ecosystem looks like, and how this network, among other crypto networks, fits into the current 
-        state of the crypto ecosystem. I would say no more than 6 paragraphs for this token, 
-        explaining what it is, what its core features are, and any negative information about 
-        it--but be sure to include ample information, especially regarding the fingerprint. We want a really good, unbiased, and 
-        illuminating view into this specific token. This report will be a single part of a greater collage 
-        of reports which will be aggregated together to aim to explore hundreds of different tokens/blockchains.
-
-        Some relevant links:
-        {row['urls']}
-
-        """
-        print(prompt)
-
-        response = gpt5_web(prompt)
-        print(response)
-
-        # upload response to cmcnotes.note
-        sql = \
-"""
-UPDATE cmcnotes
-SET note = :note
-WHERE id = :id
-"""
-        conn = engine.connect()
-        conn.execute(text(sql), {"note": response, "id": row['id']})
-        conn.commit()
-        conn.close()
-        print(f"Uploaded response to cmcnotes.note for {row['name']}")
-
-        # break
-
-    return rows
-
-
-# rows = seed_notes()
 
 
 # pprint(rows)
@@ -95,7 +23,7 @@ SELECT *
 FROM cmcnotes
 WHERE webprint_gpt5mini IS NULL
 ORDER BY rank ASC
-LIMIT 5
+LIMIT 10
 """
     # connect and return a list[dict]
     conn = engine.connect()
@@ -162,7 +90,140 @@ WHERE id = :id
 rows = hydrate_cmcnotes()
 print(f"updated {len(rows)} ids")
 pprint(rows)
+print("done")
 
 
 
+# %%
+
+
+def hydrate_webprint_distill1s():
+    sql = \
+"""
+SELECT *
+FROM cmcnotes
+WHERE webprint_distill1s IS NULL
+AND webprint_gpt5mini IS NOT NULL
+ORDER BY rank ASC
+LIMIT 500
+"""
+    conn = engine.connect()
+    rows = conn.execute(text(sql))
+    rows = rows.mappings().fetchall()
+    conn.close()
+    print(f"got: {str([row['name'] for row in rows])} fresh from db")
+
+    for row in rows:
+
+        prompt = f"""
+        In only one sentence, describe what this crypto project is and
+        the nature of this project. Start with '[name of project] is...'.
+        Do not bother defining the ticker/symbol, unless critical to the 
+        description. Do your best to refrain from heavy use of acronyms, 
+        unless they present critical or highly relevant information.
+        Return only one sentence (~30 words). Be as succinct and vivid 
+        as possible, with as high information density per token as possible:
+        INPUT:
+        <<<
+        {row['webprint_gpt5mini']}
+        >>>
+        """
+
+        print(prompt)
+        response = gpt5(prompt)
+        print(response)
+
+        sql2 = \
+"""
+UPDATE cmcnotes
+SET webprint_distill1s = :webprint_distill1s
+WHERE id = :id
+"""
+        conn = engine.connect()
+        conn.execute(text(sql2), {"webprint_distill1s": response, "id": row['id']})
+        conn.commit()
+        conn.close()
+        print(f"Uploaded response to cmcnotes.webprint_distill1s for {row['name']}")
+
+
+    return rows
+
+rows = hydrate_webprint_distill1s()
+print(f"updated {len(rows)} ids")
+pprint(rows)
+print("done")
+
+# %%
+
+
+
+def hydrate_webprint_distill1s_fun():
+    sql = \
+"""
+SELECT *
+FROM cmcnotes
+WHERE webprint_distill1s_fun IS NULL
+AND webprint_gpt5mini IS NOT NULL
+ORDER BY rank ASC
+LIMIT 100
+"""
+    conn = engine.connect()
+    rows = conn.execute(text(sql))
+    rows = rows.mappings().fetchall()
+    conn.close()
+    print(f"got: {str([row['name'] for row in rows])} fresh from db")
+
+    for row in rows:
+
+        prompt = f"""
+        In only one sentence, describe what this crypto project is and
+        the nature of this project. Start with '[name of project] is...'.
+        Do not bother defining the ticker/symbol, unless critical to the 
+        description. Imbue your response with sublime and profound humor,
+        illuminating the nature of the project, surfacing any irony,
+        using sarcasm and wit to poke holes at stupid or naive assumptions,
+        and using reverence and admiration for projects which are genuinely
+        revolutionary or innovative, keeping in mind the timeframe in which
+        these innovations were introduced.
+        Do your best to refrain from heavy use of acronyms, 
+        unless they present critical or highly relevant information.
+        Return only one sentence (~30 words). Be as succinct and vivid 
+        as possible, with as high information density per token as possible.
+
+        In order to come up with the best possible response, come up with 5
+        alternate responses, ponder each one, and then select the best one 
+        from those 5 based off of (1, primarily) the quality of the information it 
+        provides, and (2, secondarily), the quality, nuance, and depth of the humor it presents.
+
+
+        INPUT:
+        <<<
+        {row['webprint_gpt5mini']}
+        >>>
+        """
+
+
+        print(prompt)
+        response = gpt5(prompt)
+        print(response)
+
+        sql2 = \
+"""
+UPDATE cmcnotes
+SET webprint_distill1s_fun = :webprint_distill1s_fun
+WHERE id = :id
+"""
+        conn = engine.connect()
+        conn.execute(text(sql2), {"webprint_distill1s_fun": response, "id": row['id']})
+        conn.commit()
+        conn.close()
+        print(f"Uploaded response to cmcnotes.webprint_distill1s_fun for {row['name']}")
+
+
+    return rows
+
+rows = hydrate_webprint_distill1s_fun()
+print(f"updated {len(rows)} ids")
+pprint(rows)
+print("done")
 # %%
