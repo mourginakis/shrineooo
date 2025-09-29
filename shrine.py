063f9e5ce8fb18cc3a259d6315e336998d830bf7
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2025-09-29 10:58:17 -0700

    make hydrate fn concurrent

--- shrine.py
+++ shrine.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3.12
 
 #%% ========================================
+import time
 from pprint import pprint
+from concurrent.futures import ThreadPoolExecutor
 from sqlalchemy import create_engine, text
 
 from src.api_chatbot import gpt5_web, gpt5_web_flex_mini, gpt5
@@ -156,25 +158,27 @@ print("done")
 # %%
 
 
-
-def hydrate_webprint_distill1s_fun():
-    sql = \
-"""
-SELECT *
-FROM cmcnotes
-WHERE webprint_distill1s_fun IS NULL
-AND webprint_gpt5mini IS NOT NULL
-ORDER BY rank ASC
-LIMIT 100
-"""
+def hydrate_webprint_distill1s_fun() -> int:
+    # concurrent hydration version
+    # retrieve some null rows to work on
+    t0 = time.time()
+    sql = """
+    SELECT *
+    FROM cmcnotes
+    WHERE webprint_distill1s_fun IS NULL
+    AND webprint_gpt5mini IS NOT NULL
+    ORDER BY rank ASC
+    LIMIT 200
+    """
     conn = engine.connect()
     rows = conn.execute(text(sql))
     rows = rows.mappings().fetchall()
     conn.close()
-    print(f"got: {str([row['name'] for row in rows])} fresh from db")
+    print(f"retrieved {len(rows)} rows from db...")
 
-    for row in rows:
-
+    # define the worker
+    def worker(row: dict) -> None:
+        # make request to inference api
         prompt = f"""
         In only one sentence, describe what this crypto project is and
         the nature of this project. Start with '[name of project] is...'.
@@ -195,35 +199,39 @@ LIMIT 100
         from those 5 based off of (1, primarily) the quality of the information it 
         provides, and (2, secondarily), the quality, nuance, and depth of the humor it presents.
 
-
         INPUT:
         <<<
         {row['webprint_gpt5mini']}
         >>>
         """
-
-
-        print(prompt)
         response = gpt5(prompt)
-        print(response)
 
-        sql2 = \
-"""
-UPDATE cmcnotes
-SET webprint_distill1s_fun = :webprint_distill1s_fun
-WHERE id = :id
-"""
+        # insert into db
+        # engine.connect() is safe across threads bc it pulls from a pool.
+        # (don't use the same instance across threads)
+        sql2 = """
+        UPDATE cmcnotes
+        SET webprint_distill1s_fun = :webprint_distill1s_fun
+        WHERE id = :id
+        """
         conn = engine.connect()
         conn.execute(text(sql2), {"webprint_distill1s_fun": response, "id": row['id']})
         conn.commit()
         conn.close()
-        print(f"Uploaded response to cmcnotes.webprint_distill1s_fun for {row['name']}")
+        print(f".", end="", flush=True)
 
 
-    return rows
+    # run the workers concurrently (yes this works in ipython)
+    rows = [dict(r) for r in rows]
+    with ThreadPoolExecutor(max_workers=10) as ex:
+        # yes this will surface errors
+        list(ex.map(worker, rows))
+    dt = time.time() - t0
+    print(f"done!")
+    return dt
 
-rows = hydrate_webprint_distill1s_fun()
-print(f"updated {len(rows)} ids")
-pprint(rows)
-print("done")
-# %%
+
+elapsed = hydrate_webprint_distill1s_fun()
+print(f"elapsed: {elapsed:.2f}")
+
+#%%
